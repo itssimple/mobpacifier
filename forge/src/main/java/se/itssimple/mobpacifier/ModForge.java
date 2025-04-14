f4299b32f1889aabde095f35f3a2a18df199f0fd
NoLifeKing <github-things@nolifeking85.tv>
2025-04-14 19:37:59 +0200

    chore: Yeet the data things.. let's think of a better way for this

--- forge/src/main/java/se/itssimple/mobpacifier/ModForge.java
+++ forge/src/main/java/se/itssimple/mobpacifier/ModForge.java
@@ -1,9 +1,6 @@
 package se.itssimple.mobpacifier;
 import se.itssimple.mobpacifier.util.Reference;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Reference.MOD_ID)
 public class ModForge {
@@ -11,4 +8,5 @@ public class ModForge {
 	public ModForge() {
 		ModCommon.init();
 	}
+
 }
